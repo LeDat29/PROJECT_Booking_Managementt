92450b55a690cc266eb7a296e55a3e138533bd86
Le Duc Dat <132889596+LeDat29@users.noreply.github.com>
2024-08-06 11:38:25 +0700

    upload origin project

---
+++ OnlineOrder/OnlineOrder/Customer/CustomerHome.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OnlineOrder.Customer
+{
+    /// <summary>
+    /// Interaction logic for CustomerHome.xaml
+    /// </summary>
+    public partial class CustomerHome : Window
+    {
+        public CustomerHome()
+        {
+            InitializeComponent();
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
