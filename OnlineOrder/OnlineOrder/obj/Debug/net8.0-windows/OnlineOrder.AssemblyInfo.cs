bdde9ca177e67ef2db2cdc8cc28f7fcc3d766636
Le Duc Dat <datldhe171371@fpt.edu.vn>
2024-08-06 13:47:06 +0700

    fix: fix bug .vs permission denied

--- OnlineOrder/OnlineOrder/obj/Debug/net8.0-windows/OnlineOrder.AssemblyInfo.cs
+++ OnlineOrder/OnlineOrder/obj/Debug/net8.0-windows/OnlineOrder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineOrder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92450b55a690cc266eb7a296e55a3e138533bd86")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineOrder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineOrder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
